02df60fc835d932d09f02d6a009370a7ba885e54
logeshdeveloper <logeshdeveloper8@gmail.com>
2020-12-29 12:28:21 +0530

    settingup database

--- Server/index.js
+++ Server/index.js
@@ -14,5 +14,10 @@ app.use(cors)
 const CONNECTION_URL = "mongodb+srv://root:root@cluster0.7ifds.mongodb.net/test?retryWrites=true&w=majority"
 const PORT = process.env.PORT || 5000
 
-mongoose.connect()
+
+mongoose.connect(CONNECTION_URL , { useNewUrlParser: true , useUnifiedTopology: true} )
+    .then(app.listen(PORT , () => console.log(`Server running on port ${PORT}`)))
+    .catch((error) => console.log(error.message))
+
+mongoose.set('useFindAndModify', false);
 

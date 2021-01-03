1310769c5758549cbe88706cafd09f8e15a608eb
logeshdeveloper <logeshdeveloper8@gmail.com>
2021-01-03 22:26:03 +0530

    server configured

--- Server/controllers/posts.js
+++ Server/controllers/posts.js
@@ -1,3 +1,28 @@
-export const getPosts  = (req , res) => {
-    res.send('Server Started');
+import PostMessage from '../models/postMessage.js'
+
+
+export const getPosts  = async (req , res) => {
+    try {
+        const postMessage = await PostMessage.find()
+
+        res.status(202).json(postMessage);
+    } catch (error) {
+        res.status(404).json({messsage : error.messsage});
+    }    
 };
+
+export const createPosts = (req , res) =>{
+
+    const post = req.body;
+    const createPost = new PostMessage(post);
+
+     try {
+         //Saving the post request which is aquired through req.body inside createPost Variable 
+         createPost.save();
+
+         res.status(202).json(createPost);
+
+     } catch (error) {
+        res.status(404).json({messsage : error.messsage});
+     }
+}
